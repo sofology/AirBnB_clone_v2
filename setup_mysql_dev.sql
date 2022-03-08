9722e7f33137883ea968ec5a9d0bb5ac24ec4f5f
Anteneh Kassaw <88293613+Anteneh2121@users.noreply.github.com>
2022-03-08 00:12:36 -0800

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,7 @@
--- Creating hbnb_dev_db database
--- Privileges for new user (hbnb_dev)
+-- prepares a MySQL server for the project
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-USE hbnb_dev_db;
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
